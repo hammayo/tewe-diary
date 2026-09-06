bfa46c88457b4afb1229cac7fdf3fc15a95cf8b3
Hammy Babar <hammy@hammayo.co.uk>
2026-09-06 14:47:24 +0100

    Added initial shell movies CRUD endpoints.

---
+++ Movies.Api/Program.cs
@@ -0,0 +1,28 @@
+using Movies.Application;
+using Movies.Application.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
